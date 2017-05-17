f0ae3dc8206a03cfe15c57ce3eda25c5d0bad020
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-17 22:22:07 +0000

    Ajax tweet requests from server implemented, error messages when tweet is invalid, reversed order of tweet sort function serverside.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,55 +1,10 @@
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
 $(document).on("ready", () => {
 
   function createTweetElement(tweet) {
@@ -71,15 +26,57 @@ $(document).on("ready", () => {
     return tweetElement
   }
 
-
   function renderTweets(jsonData) {
-
+    $("#tweets").empty();
     jsonData.forEach( (userObj) =>{
       $('#tweets').append(createTweetElement(userObj));
     })
   }
 
-  renderTweets(data);
+  function loadTweets() {
+    $.ajax( {
+        method: "GET",
+        url: "/tweets",
+    }).done(renderTweets)
+  }
+
+
+  function addErrorMSG(errorSTR ) {
+    let errMSG = $("<div>").addClass("errMSG").text(errorSTR);
+    $(".new-tweet").after(errMSG);
+
+  }
+
+  loadTweets();
+
+  $(".new-tweet").on("submit", "form", function(event) {
+    event.preventDefault();
+    $(".errMSG").remove();
+    let text = $("textarea").val();
+    let trimText = $("textarea").val().trim();
+    if (text.length > 140) {
+      addErrorMSG("Too many things!");
+    } else if (trimText.length === 0) {
+      addErrorMSG("Say some more, please.");
+    } else {
+      $.ajax( {
+        url: "/tweets",
+        method: "POST",
+        data: $(this).serialize()
+      }).done( () => {
+        $("textarea").val("");
+        loadTweets();
+      })
+    }
+  })
 
 
 })
+
+
+
+
+
+
+
+
