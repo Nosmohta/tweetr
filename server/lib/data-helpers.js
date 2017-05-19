5e928198baec8661ce2a736f4c0775b416c7d0d0
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-19 01:05:09 +0000

    mogoDB added. getTweets now pulls from mongoDB and not local files.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -17,11 +15,25 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => b.created_at - a.created_at ;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      console.log("about to create tweetsAry")
+
+      db.collection("tweets").find({}).toArray((err , results) => {
+        if (err) {
+          console.log(err);
+        }
+
+        let tweetsAry = results;
+        console.log(tweetsAry);
+
+        const sortNewestFirst = (a, b) => b.created_at - a.created_at;
+        callback(null, tweetsAry.sort(sortNewestFirst));
+
+
+
+      })
     }
 
   };
+
 }
+
